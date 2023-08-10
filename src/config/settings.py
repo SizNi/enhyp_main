8d4fad510d07bfa66857d77ad611938d87725c7c
SizNi <wwwwwwwww@list.ru>
2023-08-10 19:37:07 +0300

    добавил login url и настроил

--- src/config/settings.py
+++ src/config/settings.py
@@ -82,10 +82,15 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 MEDIA_URL = "/media/"
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
-STATIC_URL = "/static/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_URL = "static/"
+
+LOGIN_URL = "user_login"
+
+LOGIN_REDIRECT_URL = "user_login"
 
 DATABASES = {
     "default": {
@@ -129,7 +134,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
