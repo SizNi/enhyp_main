cdaad716b4d93aeb86dfda8cae9cd841cebed585
SizNi <wwwwwwwww@list.ru>
2023-10-08 22:36:05 +0300

    typo

--- src/config/settings.py
+++ src/config/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["77.232.134.147:8000", "127.0.0.1"]
+ALLOWED_HOSTS = ["77.232.134.147", "127.0.0.1"]
 
 
 # Application definition
